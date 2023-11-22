9ff5cb0576227c9c4b6b9f8ca0b744c75bd8efc8
Anupama Murthi <Anupama.Murthi@ibm.com>
2023-11-22 13:10:34 -0500

    try

--- judgelm/llm_judge/gen_model_judgement.py
+++ judgelm/llm_judge/gen_model_judgement.py
@@ -163,7 +163,7 @@ def get_model_answers(
 
         # Dump answers
         os.makedirs(os.path.dirname(answer_file), exist_ok=True)
-        with open(os.path.expanduser(answer_file), "a") as fout:
+        with open(os.path.expanduser(answer_file + ".new"), "a") as fout:
             ans_id = shortuuid.uuid()
             question["pred_id"] = ans_id
             question["pred_text"] = output
